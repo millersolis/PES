af94f7d5c7b95c7230c0824befc211cfa6ea7c6f
oliviakuninaka <oliviakuninaka@gmail.com>
2022-08-06 20:59:01 -0700

    Added DB to sim ecu

--- MainECUSimulator/testapp.py
+++ MainECUSimulator/testapp.py
@@ -2,6 +2,30 @@ import tkinter as tk
 import can
 import time
 import threading
+import sys
+import os
+from tkinter import font
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../DB'))
+import create_sqlite_db 
+
+OUTPUTPATH = '../DB/sqlite_dbs'
+SQLITE_FILENAME = 'echo_db.db'
+# Assumme PDM public key is b06188d2-65d6-4ffb-b9b1-578b0e35e002
+MOTORCYCLE_PDM = 'b06188d2-65d6-4ffb-b9b1-578b0e35e002'
+
+sqlite_db_path = os.path.join(OUTPUTPATH, SQLITE_FILENAME)
+
+class Database:
+    def get_enrollment_table(self, sqlite_db_path, motorcycle_pdm):
+        
+        conn = create_sqlite_db.create_connection(sqlite_db_path)
+
+        with conn:
+
+            json_enrollment_table = create_sqlite_db.get_enrollment_table(conn, motorcycle_pdm)
+            
+            return json_enrollment_table
 
 
 class SenderReceiver:
@@ -11,20 +35,25 @@ class SenderReceiver:
         # Code to format can message data here
         msg = can.Message(arbitration_id=0xffff, data=[10, 1, 0, 1, 3, 1, 4, 1], is_extended_id=False)
         self.bus.send(msg)
-
-    def send_enrollment_table_to_pdm(self):
+        
+    def send_enrollment_table_to_pdm(self, db):
         # Code to format JSON response from DB here
+        json_enrollment_table = db.get_enrollment_table(sqlite_db_path, MOTORCYCLE_PDM)
+        print(json_enrollment_table)
+    
+        
         msg = can.Message(arbitration_id=0xffff, data=[10, 1, 0, 1, 3, 1, 4, 1], is_extended_id=False)
         self.bus.send(msg)
        
     def recv_can_message(self, app):
         while True:
             message = self.bus.recv()
-
+                        
             # Code to interpret received message data and call proper function here
-            app.change_state_label()
+            state = 1
+            app.change_state_label(state)
             
-    def __init__(self):
+    def __init__(self):        
         # Note that channel will change depending on the port
         # bus = can.interface.Bus(bustype='seeedstudio', channel='com4', bitrate=500000)
         # bus = can.Bus(bustype='seeedstudio', channel='com4', bitrate=500000, operation_mode='normal')
@@ -35,6 +64,8 @@ class App(tk.Tk):
   
     def __init__(self, sender):   
         super().__init__()  
+
+        self.db = Database()
         
         self.geometry('900x600')
         self.columnconfigure([0,1], weight=1, minsize=75)
@@ -61,7 +92,7 @@ class App(tk.Tk):
         self.lock_label = tk.Label(master=states_frame, text='STOP', fg='black', bg='#e2c3b8', width=25, height=8)
         self.lock_label.pack()
 
-        self.start_button = tk.Button(text='Send enrollment table', fg='black', bg='green', width=40, height=3, command=sender.send_enrollment_table_to_pdm)      
+        self.start_button = tk.Button(text='Send enrollment table', fg='black', bg='green', width=40, height=3, command=lambda: sender.send_enrollment_table_to_pdm(self.db))      
         self.start_button.grid(row=1, column=1)
         
         # Not sure if a text box should be used for the DB part but putting it here for now
@@ -69,9 +100,27 @@ class App(tk.Tk):
         self.cmd_text_box.grid(row=2, column=1)
         
 
-    def change_state_label(self):
-        # Change bike state labels here
-        self.wakeup_label.configure(bg='yellow')
+    def change_state_label(self, state):
+        
+        # state values can be changed later
+        if state == 1:
+            self.wakeup_label.configure(bg='yellow', font=font.Font(weight='bold'))
+            self.start_label.configure(bg='#cffcd2', font=font.Font(weight='normal'))
+            self.lock_label.configure(bg='#e2c3b8', font=font.Font(weight='normal'))
+            
+            self.start_button.configure(bg='green', font=font.Font(weight='bold'))
+        elif state == 2:
+            self.wakeup_label.configure(bg='#fcfccf', font=font.Font(weight='normal'))
+            self.start_label.configure(bg='green', font=font.Font(weight='bold'))
+            self.lock_label.configure(bg='#e2c3b8', font=font.Font(weight='normal'))
+            
+            self.start_button.configure(bg='#cffcd2', font=font.Font(weight='normal'))
+        else:
+            self.wakeup_label.configure(bg='#fcfccf', font=font.Font(weight='normal'))
+            self.start_label.configure(bg='#cffcd2', font=font.Font(weight='normal'))
+            self.lock_label.configure(bg='red', font=font.Font(weight='bold'))
+        
+            self.start_button.configure(bg='#cffcd2', font=font.Font(weight='normal'))
 
   
 if __name__ == "__main__":
