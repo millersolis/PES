fb68f2ea0f90b326c84b02bf4062d9daacb93e35
Shawn Baltar <shawnnbaltar@gmail.com>
2022-08-07 12:07:19 -0700

    add PDM control messages

--- PDM/CAN_test1/Core/Inc/main.h
+++ PDM/CAN_test1/Core/Inc/main.h
@@ -59,6 +59,7 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define Btn_Pin GPIO_PIN_13
 #define Btn_GPIO_Port GPIOC
+#define Btn_EXTI_IRQn EXTI15_10_IRQn
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
